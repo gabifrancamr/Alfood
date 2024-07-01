5d5f9836a400ee36b52850b802bd15eff24c92da
Gabriela <gabifmouraroc@outlook.com>
2024-07-01 10:15:48 -0300

    paginação

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,25 +1,54 @@
-import { useEffect, useState } from 'react';
-import IRestaurante from '../../interfaces/IRestaurante';
-import style from './ListaRestaurantes.module.scss';
-import Restaurante from './Restaurante';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import IRestaurante from "../../interfaces/IRestaurante";
+import style from "./ListaRestaurantes.module.scss";
+import Restaurante from "./Restaurante";
+import axios from "axios";
+import { IPaginacao } from "../../interfaces/IPaginacao";
 
 const ListaRestaurantes = () => {
-
-  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState("");
 
   useEffect(() => {
     // obter restaurantes
-    axios.get('http://localhost:8000/api/v1/restaurantes/').then(resposta => {
-      // console.log(resposta)
-      setRestaurantes(resposta.data.results)
-    }).catch(erro => { console.log(erro) })
-  }, [])
+    axios
+      .get<IPaginacao<IRestaurante>>(
+        "http://localhost:8000/api/v1/restaurantes/"
+      )
+      .then((resposta) => {
+        // console.log(resposta)
+        setRestaurantes(resposta.data.results);
+        setProximaPagina(resposta.data.next);
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  }, []);
 
-  return (<section className={style.ListaRestaurantes}>
-    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
-    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
-  </section>)
-}
+  const verMais = () => {
+    axios
+      .get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then((resposta) => {
+        // console.log(resposta)
+        setRestaurantes([...restaurantes, ...resposta.data.results]);
+        setProximaPagina(resposta.data.next);
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  };
 
-export default ListaRestaurantes
+  return (
+    <section className={style.ListaRestaurantes}>
+      <h1>
+        Os restaurantes mais <em>bacanas</em>!
+      </h1>
+      {restaurantes?.map((item) => (
+        <Restaurante restaurante={item} key={item.id} />
+      ))}
+      {proximaPagina && <button onClick={verMais}>ver mais</button>}
+    </section>
+  );
+};
+
+export default ListaRestaurantes;
